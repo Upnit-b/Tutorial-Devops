36711263111a6e6c62700a907f9d6370960616a7
Upnit-b <upnit.singh@gmail.com>
2025-11-08 12:39:45 -0500

    added some changes in server.js for dev

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Shutting down gracefully...');
+  console.log('Changed for dev - Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
